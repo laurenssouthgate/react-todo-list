5d445f8e94308d6398e601d5225a1af6363e3afe
Laurens <laurenssouthgate@gmail.com>
2025-04-25 10:46:31 +0100

    basic functionality of task row done

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,41 @@ import './App.css'
 import {Task} from "./model/Task.tsx";
 import _LocalStorage from "./util/LocalStorage.tsx";
 import TaskRow from "./component/TaskRow.tsx";
+import {useEffect, useState} from "react";
 
 
 function App() {
-    const tasks : Task[] | null = _LocalStorage.loadTasks()
+    const [tasks, setTasks] = useState<Task[] | null>(null)
 
-  return (
-      <div className="main">
-          <h1>Todo List</h1>
-          {
-              tasks === null &&
-                  <div className="no-tasks">
-                      <span>You do not currently have any tasks saved, click the button below to add a new task</span>
-                  </div>
-          }
-          {
-              tasks !== null &&
-              tasks.map(task => (
-                <TaskRow task={ task } key={ task.id } />
-              ))
-          }
-      </div>
-  )
+    useEffect(() => {
+        const loadedTasks = _LocalStorage.loadTasks()
+
+        if (loadedTasks !== null) {
+            setTasks(loadedTasks)
+        }
+    }, [])
+
+    return (
+        <div className="main">
+            <h1>Todo List</h1>
+            {
+                tasks === null ? (
+                    <div className="no-tasks">
+                        <span>You do not currently have any tasks saved, click the button below to add a new task</span>
+                    </div>
+                ) : (
+                    <div className="tasks">
+                        {
+                            tasks.map(task => (
+                                <TaskRow task={task} key={task.id}/>
+                            ))
+                        }
+                    </div>
+                )
+
+            }
+        </div>
+    )
 }
 
 export default App
--- src/component/TaskRow.tsx
+++ src/component/TaskRow.tsx
@@ -1,12 +1,59 @@
 import {Task} from "../model/Task.tsx";
+import {ChangeEvent, useState} from "react";
 
 type TaskRowProps = {
     task: Task
+    onComplete: (id: number) => void
+    onEdit: (id: number, updatedText: string) => void
 }
-export default function TaskRow({ task } : TaskRowProps) {
+export default function TaskRow({ task, onComplete, onEdit } : TaskRowProps) {
+    const [editing, setEditing] = useState<boolean>(false)
+    const [updatedText, setUpdatedText] = useState<string>(task.text)
+
+    const changeHandler = (e : ChangeEvent<HTMLInputElement>) => {
+        setUpdatedText(e.target.value)
+    }
+
+    const handleComplete = () => {
+        onComplete(task.id)
+    }
+
+    const handleEdit = () => {
+        setEditing(editing => !editing)
+
+        if (editing) {
+            onEdit(task.id, updatedText)
+        }
+    }
+
     return (
-        <div className="task">
-            <span className="text">{ task.text }</span>
+        <div className="task" data-completed={ task.completed }>
+            {
+                editing ?
+                    (
+                        <input
+                            type="text"
+                            id="text"
+                            value={ updatedText }
+                            onChange={ changeHandler }
+                        />
+                    ) : (
+                        <span className="text">{ task.text }</span>
+                    )
+            }
+            //TODO add icons to buttons
+            <button
+                type="button"
+                data-action="mark-done"
+                onClick={ handleComplete }>
+                Completed
+            </button>
+            <button
+                type="button"
+                data-action="edit"
+                onClick={ handleEdit }>
+                Edit
+            </button>
         </div>
     )
 }
